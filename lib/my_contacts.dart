72d5403c727bdd986c573f302283153e9ebf62e9
Abdelrahmen-Hatem <bodyhatem24@gmail.com>
2025-02-27 21:31:32 +0200

    modified Grid

--- lib/my_contacts.dart
+++ lib/my_contacts.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:my_contact_app/widgets/Social_Media_icons.dart';
+import 'package:my_contact_app/widgets/contactsGrid.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 // ignore: must_be_immutable
 class MyContacts extends StatelessWidget {
   MyContacts({super.key});
-  
-  Map<String,String> socialMedia = {
-  'facebook.png':  'https://www.facebook.com/abdelrhman.hatem.199',
-    'whatsapp.png':'https://wa.me/201063370030',
-    'instagram.png':'https://www.instagram.com/abdulraman.hatem/?__pwa=1',
+
+  Map<String, String> socialMedia = {
+    'facebook.png': 'https://www.facebook.com/abdelrhman.hatem.199',
+    'whatsapp.png': 'https://wa.me/201063370030',
+    'instagram.png': 'https://www.instagram.com/abdulraman.hatem/?__pwa=1',
     'telegram.png': 'https://t.me/Glliittch',
     'twitter.png': 'https://x.com/abdelravman',
-    'github.png':'https://github.com/Abdelrahmen-Hatem',
-};
+    'github.png': 'https://github.com/Abdelrahmen-Hatem',
+  };
   @override
   Widget build(BuildContext mycontext) {
     return Scaffold(
@@ -65,27 +65,13 @@ class MyContacts extends StatelessWidget {
                       color: Colors.grey,
                     ),
                     onPressed: () {
-                     launchUrl(Uri.parse('tel:+201063370030'));
-
+                      launchUrl(Uri.parse('tel:+201063370030'));
                     },
                   ),
                 ],
               ),
               SizedBox(height: 20),
-              GridView.builder(
-                itemCount: socialMedia.length ,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                ),
-                itemBuilder: (mycontext, index) {
-                 
-                    return SocialMediaIcon(SocialMedia: socialMedia.keys.toList()[index],socialMediaLinks: socialMedia.values.toList()[index]);
-                  
-                },
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                padding: EdgeInsets.all(20),
-              ),
+              ContactsGrid(socialMedia: socialMedia),
             ],
           ),
         ),
---
+++ lib/widgets/contactsGrid.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/widgets.dart';
+import 'package:my_contact_app/widgets/Social_Media_icons.dart';
+
+class ContactsGrid extends StatelessWidget {
+  const ContactsGrid({
+    super.key,
+    required this.socialMedia,
+  });
+
+  final Map<String, String> socialMedia;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: socialMedia.length ,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+      ),
+      itemBuilder: (mycontext, index) {
+       
+          return SocialMediaIcon(SocialMedia: socialMedia.keys.toList()[index],socialMediaLinks: socialMedia.values.toList()[index]);
+        
+      },
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      padding: EdgeInsets.all(20),
+    );
+  }
+}
